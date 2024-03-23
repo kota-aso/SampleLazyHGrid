5a23506407eca77d4654e789ea5df5a734fee49c
kota-aso <kotaaso1212@gmail.com>
2024-03-23 09:50:52 +0900

    Initial Commit

---
+++ SampleLazyHGrid/SampleLazyHGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLazyHGridApp.swift
+//  SampleLazyHGrid
+//
+//  Created by Kota Aso on 2024/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleLazyHGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
